c18e1ffde1662b97c2782e594971864a6cf985ee
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-11-08 22:15:00 -0500

    updated

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production ") {
     return {
       upload: {
-        provider: "aws-s3",
+        provider: "strapi-provider-upload-aws-s3",
         providerOptions: {
           accessKeyId: env("AWS_ACCESS_KEY_ID"),
           secretAccessKey: env("AWS_ACCESS_SECRET"),
@@ -16,3 +16,8 @@ module.exports = ({ env }) => {
   }
   return {};
 };
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "aws-s3",
+  },
+});
